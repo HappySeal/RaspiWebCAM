4680279dff3a6edaf33632a3e64c47cca8eea5af
HappySeal <cafer15165@gmail.com>
2017-09-08 14:31:15 +0300

    V 1.0.4

--- cam.py
+++ cam.py
@@ -7,6 +7,6 @@ app.debug = True
 @app.route("/")
 def cam():
 	os.system("fswebcam -r 640x480 --no-banner static/images/webcam.jpg")
-	return render_template('cam.html' , pic="webcam.jpg")
+	return render_template('cam.html' , pic="static/images/webcam.jpg")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
