1013374db063c4ff22fff12cf6514f467ada77d8
HappySeal <cafer15165@gmail.com>
2017-09-08 14:16:23 +0300

    Initial commit

---
+++ cam.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+import os
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def cam():
+	os.system("fswebcam -r 640x480 --no-banner static/images/webcam.jpg")
+	return render_template('cam.html' , pic="webcam.jpg")
